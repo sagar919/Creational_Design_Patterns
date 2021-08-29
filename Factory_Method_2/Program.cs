6dee272aa45c135acfff0b568c1db99d6e6f9935
Sagar Lad <sagar.lad@morningstar.com>
2021-08-29 11:37:17 +0530

    Added Welcome Message

---
+++ Factory_Method_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory_Method_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
