9e71ab78d807345594ee51c941e89b9df20a7a22
Sagar Lad <sagar.lad@morningstar.com>
2021-08-29 18:01:41 +0530

    Added Singleton design pattern

--- Singleton1/Program.cs
+++ Singleton1/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Singleton1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            Singleton teacher = Singleton.GetInstance;
+            teacher.printDetails("this is Teacher");
+
+            Singleton student = Singleton.GetInstance;
+            student.printDetails("this is Student");
+
+        }
+
+        public sealed class Singleton
+        {
+            private static Singleton instance = null;
+            public static Singleton GetInstance
+            {
+                get
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+
+            private Singleton()
+            {
+            }
+
+            public void printDetails(string message)
+            {
+                Console.WriteLine(message);
+            }
         }
     }
 }
+
